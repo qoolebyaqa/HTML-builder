d6e6c71d05815ad879c4d30d2cfd85df10598ce4
Ten <Qoolebyaqa@gmail.com>
2022-11-05 23:14:04 +0500

    feat: add copying of files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,60 @@
+const path = require('path');
+const fs = require('fs');
+
+
+const pathUse = __dirname+'/files';
+const dublicateF = __dirname+'/files-copy'
+fs.stat(dublicateF, err => {
+    if (!err) {
+        console.log('Folder had been already created, the included files were updated');
+
+        fs.readdir(pathUse, (err, files) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                files.forEach((value) => {
+                    let oldPath = pathUse + '/' + value;
+                    let newPath = dublicateF + '/' + value;
+
+                    fs.copyFile(oldPath, newPath, (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                    })
+                });
+            }
+        });
+    }
+    else {
+        fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+            if (err) {
+                throw err;
+            }
+        });
+
+        fs.readdir(pathUse, (err, files) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                files.forEach((value) => {
+                    let oldPath = pathUse + '/' + value;
+                    let newPath = dublicateF + '/' + value;
+
+                    fs.copyFile(oldPath, newPath, (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                    })
+                });
+            }
+        });
+        console.log('Files were copied to new folder')
+    }
+    
+})
+
+
+
 

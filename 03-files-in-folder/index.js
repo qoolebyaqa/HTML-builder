0712821e6f9da2fca33c475bc0897e59e16eeefe
Ten <Qoolebyaqa@gmail.com>
2022-11-05 21:45:12 +0500

    feat: add the looping in the secret folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathUse = __dirname+'/secret-folder';
+
+
+
+fs.readdir(pathUse, (err, files) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        files.forEach((value) => {
+            let fileCurrent = pathUse + '/' + value;
+            let kb;
+            fs.stat(fileCurrent, (err, stats) => {
+                if (err) {
+                    return;
+                }
+                else {
+                    if (stats.isDirectory()) {
+                        let nameF = value;
+                        kb = stats.size;
+                        console.log(`${nameF} - folder - ${(kb/1024).toFixed(2) + ' kb'}`);
+                    }
+                    else {
+                        let nameF = value.slice(0, value.lastIndexOf('.'));
+                        kb = stats.size;
+                        console.log(`${nameF} - ${path.extname(value).slice(1)} - ${(kb/1024).toFixed(2) + ' kb'}`);                        
+                    }                    
+                }
+            })
+            
+        });
+    }
+});
